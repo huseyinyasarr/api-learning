7530c0eb03cc2f88f2f1a8d8d3e55334c52fb8fa
huseyinyasarr <h.yasar.tr@gmail.com>
2024-09-15 02:06:07 +0300

    gitignore added

---
+++ HelloApi/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using HelloApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloApi.Controllers;
+
+[ApiController]
+[Route("home")]
+
+public class HomeController : ControllerBase
+{
+    [HttpGet]
+
+    public ResponseModel GetHello()
+    {
+        return new ResponseModel()
+        {
+            httpStatus = 200,
+            message = "Selamunaleyküm"
+        };
+    }
+}
---
+++ HelloApi/Models/ResponseModel.cs
@@ -0,0 +1,7 @@
+﻿namespace HelloApi.Models;
+
+public class ResponseModel
+{
+    public int httpStatus { get; set; }
+    public string message { get; set; }
+}
