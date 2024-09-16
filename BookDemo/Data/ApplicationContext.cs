843d9f34986ff3dac7d8dfb346e3d2fe77d0b299
huseyinyasarr <h.yasar.tr@gmail.com>
2024-09-16 13:14:50 +0300

    static data (ApplicationContext) added for BookDemo

--- BookDemo/Data/ApplicationContext.cs
+++ BookDemo/Data/ApplicationContext.cs
@@ -1,7 +1,13 @@
-﻿namespace BookDemo.Data;
+﻿using BookDemo.Models;
+
+namespace BookDemo.Data;
 
 public static class ApplicationContext
 {
+    //bu proje için statik veri olarak kullanılacak
+
+    public static List<Book> Books { get; set; }
+
     static ApplicationContext()
     {
 
