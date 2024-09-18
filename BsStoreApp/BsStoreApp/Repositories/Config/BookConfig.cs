adeb890119667e689b300d5675d2ac13c85811ea
huseyinyasarr <h.yasar.tr@gmail.com>
2024-09-18 15:43:43 +0300

    BookConfig added

---
+++ BsStoreApp/BsStoreApp/Repositories/Config/BookConfig.cs
@@ -0,0 +1,14 @@
+﻿using BsStoreApp.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace WebApi.Repositories.Config;
+
+public class BookConfig : IEntityTypeConfiguration<Book>
+{
+    public void Configure(EntityTypeBuilder<Book> builder)
+    {
+        throw new NotImplementedException();
+    }
+}
